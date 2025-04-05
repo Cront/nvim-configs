dd3252aa378df209f891a07cc7c3bd79af6b4810
Hasan <hasan0407@gmail.com>
2025-04-05 07:45:04 -0700

    Fixed some settings

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+-- Add ~/.config/nvim/lua to package.path
+package.path = package.path .. ";/home/smha/.config/nvim/lua/?.lua;/home/smha/.config/nvim/lua/?/init.lua"
 require("hasan.core")
 require("hasan.lazy")
 require("nvim-treesitter.install").prefer_git = true
--- lua/hasan/plugins/lsp/lspconfig.lua
+++ lua/hasan/plugins/lsp/lspconfig.lua
@@ -4,7 +4,7 @@ return {
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     { "antosha417/nvim-lsp-file-operations", config = true },
-    { "folke/neodev.nvim", opts = {} },
+    { "folke/neodev.nvim",                   opts = {} },
   },
   config = function()
     -- import lspconfig plugin
@@ -54,10 +54,10 @@ return {
         keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
         opts.desc = "Go to previous diagnostic"
-        keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+        keymap.set("n", "[d", vim.diagnostic.goto_prev({}), opts) -- jump to previous diagnostic in buffer
 
         opts.desc = "Go to next diagnostic"
-        keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+        keymap.set("n", "]d", vim.diagnostic.goto_next({}), opts) -- jump to next diagnostic in buffer
 
         opts.desc = "Show documentation for what is under cursor"
         keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
@@ -72,7 +72,7 @@ return {
 
     -- Change the Diagnostic symbols in the sign column (gutter)
     -- (not in youtube nvim video)
-    local signs = { Error = "❌", Warn = "⚠️ ", Hint = "💡", Info = "ℹ️ " }
+    local signs = { Error = "❌", Warn = "⚠️", Hint = "💡", Info = "ℹ️" }
     for type, icon in pairs(signs) do
       local hl = "DiagnosticSign" .. type
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
